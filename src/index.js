300879a9c9e906e7f19bd56dac1531d4726ca35c
Jade Lynn Masker <jadelynn@masker.family>
2020-12-12 11:46:20 -0500

    commit tree 3740f837aa44d9222b22fd01ee47665f2df5e2e4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+document.addEventListener('DOMContentLoaded', function() {
+	const imageContainer = document.getElementById('dog-image-container');
+
+	fetch(imgUrl).then(function(response) {
+		return response.json();
+	}).then(function(json) {
+		for (let i = 0; i < json.length; i++) {
+			const imgTag = document.createElement('img');
+			imgTag.src = json[i];
+			imageContainer.appendChild(imgTag);
+		}
+	})
+})
